f308ac7c0e7019d4dd71c8ec9eaa3dcdc8eb2881
Alexandre Amat <alex.amat@i-drc.com>
2023-05-22 16:00:38 +0200

    add account

--- backend/app/models/institution.py
+++ backend/app/models/institution.py
@@ -1,21 +1,27 @@
 from typing import TYPE_CHECKING
 
-from sqlalchemy import Column, Integer, String, CheckConstraint
-from sqlalchemy.orm import relationship, Session
-from sqlalchemy.exc import NoResultFound
-from fastapi.encoders import jsonable_encoder
+from sqlalchemy import Column, String, CheckConstraint
+from sqlalchemy.orm import Mapped, relationship
 
 import pycountry
 
 from app.models.base import Base
 
 
+if TYPE_CHECKING:
+    from . import UserInstitutionLink
+
 country_codes = (country.alpha_2 for country in pycountry.countries)
 
 
 class Institution(Base):
-    id = Column(Integer, primary_key=True, index=True)
+    __tablename__ = "institutions"
+
     name = Column(String)
     country_code = Column(String, nullable=False)
+
+    user_links: Mapped[list["UserInstitutionLink"]] = relationship(
+        "UserInstitutionLink", back_populates="institution"
+    )
 
     __table_args__ = (CheckConstraint(country_code.in_(country_codes)),)
