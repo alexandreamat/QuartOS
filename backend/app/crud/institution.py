10298103c58d4401a7e77d1825f785c248d98df2
Alexandre Amat <alex.amat@hotmail.com>
2023-05-23 11:50:36 +0200

    crud as class

--- backend/app/crud/institution.py
+++ backend/app/crud/institution.py
@@ -1,15 +1,12 @@
 from sqlalchemy.orm import Session
 
 from app import models, schemas
-from .base import CRUDFactory
+from .base import CRUDBase
 
 
-class CRUDInstitutionFactory(
-    CRUDFactory[models.Institution, schemas.InstitutionRead, schemas.InstitutionWrite]
+class CRUDInstitution(
+    CRUDBase[models.Institution, schemas.InstitutionRead, schemas.InstitutionWrite]
 ):
-    ...
-
-
-CRUDInstitution = CRUDInstitutionFactory(
-    models.Institution, schemas.InstitutionRead, schemas.InstitutionWrite
-)
+    model_type = models.Institution
+    read_schema_type = schemas.InstitutionRead
+    write_schema_type = schemas.InstitutionWrite
