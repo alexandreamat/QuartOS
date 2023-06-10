4deb4a6f416f626e7bddbef3438fb11d8df0b95b
Alexandre Amat <alex.amat@hotmail.com>
2023-06-10 17:30:00 +0200

    prefill currency, and make it not changeable

--- frontend/src/components/FormCurrencyInput.tsx
+++ frontend/src/components/FormCurrencyInput.tsx
@@ -1,46 +1,33 @@
 import useFormField from "hooks/useFormField";
-import { Form, InputOnChangeData } from "semantic-ui-react";
-import FormDropdownInput from "./FormDropdownInput";
-import { codes } from "currency-codes";
-
-const currencyCodeOptions = codes().map((code, index) => ({
-  key: index,
-  value: code,
-  text: code,
-}));
+import { Form, InputOnChangeData, Label } from "semantic-ui-react";
 
 export default function FormCurrencyInput(props: {
   label: string;
   amount: ReturnType<typeof useFormField<string>>;
-  currency: ReturnType<typeof useFormField<string>>;
+  currency: string;
 }) {
   return (
-    <Form.Group widths="equal">
-      <Form.Input
-        type="number"
-        input={{
-          inputMode: "decimal",
-          step: "0.01",
-        }}
-        label={props.label}
-        placeholder={"Enter " + props.label}
-        required
-        value={props.amount.value}
-        onChange={(
-          e: React.ChangeEvent<HTMLInputElement>,
-          data: InputOnChangeData
-        ) => {
-          props.amount.reset();
-          props.amount.set(data.value as string);
-        }}
-        error={props.amount.isError}
-      />
-      <FormDropdownInput
-        field={props.currency}
-        label="Currency"
-        options={currencyCodeOptions}
-        compact
-      />
-    </Form.Group>
+    <Form.Input
+      type="number"
+      input={{
+        inputMode: "decimal",
+        step: "0.01",
+      }}
+      placeholder={"Enter " + props.label}
+      required
+      value={props.amount.value}
+      onChange={(
+        e: React.ChangeEvent<HTMLInputElement>,
+        data: InputOnChangeData
+      ) => {
+        props.amount.reset();
+        props.amount.set(data.value as string);
+      }}
+      labelPosition="left"
+      error={props.amount.isError}
+    >
+      <Label>{props.currency}</Label>
+      <input />
+    </Form.Input>
   );
 }
