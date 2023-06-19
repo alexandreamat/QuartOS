40e25d2422ed9117e6e89432d01a27706b29dc35
Alexandre Amat <alex.amat@i-drc.com>
2023-06-19 10:45:02 +0200

    calculated fields in movements

--- backend/app/features/movement/models.py
+++ backend/app/features/movement/models.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+from datetime import datetime
 from typing import TYPE_CHECKING
 from sqlmodel import SQLModel, Relationship
 
@@ -12,8 +14,10 @@ class __MovementBase(SQLModel):
 
 
 class MovementApiOut(__MovementBase, IdentifiableBase):
-    # transactions: List[TransactionApiOut] # does not work
-    ...
+    amount: Decimal | None
+    currency_code: str | None
+    earliest_timestamp: datetime | None
+    latest_timestamp: datetime | None
 
 
 class MovementApiIn(__MovementBase):
@@ -21,4 +25,30 @@ class MovementApiIn(__MovementBase):
 
 
 class Movement(__MovementBase, IdentifiableBase, table=True):
-    transactions: "Transaction" = Relationship(back_populates="movement")
+    transactions: list["Transaction"] = Relationship(back_populates="movement")
+
+    @property
+    def amount(self) -> Decimal | None:
+        if len({t.currency_code for t in self.transactions}) != 1:
+            return None
+        return sum([t.amount for t in self.transactions], Decimal(0))
+
+    @property
+    def currency_code(self) -> str | None:
+        if len({t.currency_code for t in self.transactions}) != 1:
+            return None
+        return self.transactions[0].currency_code
+
+    @property
+    def earliest_timestamp(self) -> datetime | None:
+        try:
+            return min(t.timestamp for t in self.transactions if t.timestamp)
+        except ValueError:
+            return None
+
+    @property
+    def latest_timestamp(self) -> datetime | None:
+        try:
+            return max(t.timestamp for t in self.transactions if t.timestamp)
+        except ValueError:
+            return None
