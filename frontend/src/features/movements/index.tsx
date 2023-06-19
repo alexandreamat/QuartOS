68d350898ddef515387cc4e04424278ecc079526
Alexandre Amat <alex.amat@hotmail.com>
2023-06-19 20:40:28 +0200

    infinite query for movements

--- frontend/src/features/movements/index.tsx
+++ frontend/src/features/movements/index.tsx
@@ -1,12 +1,13 @@
 import { api } from "app/services/api";
 import FlexColumn from "components/FlexColumn";
 import { QueryErrorMessage } from "components/QueryErrorMessage";
-import { Loader } from "semantic-ui-react";
+import { Loader, Message } from "semantic-ui-react";
 import { Bar } from "./components/Bar";
 import { Movement } from "./components/Movement";
 import Form from "./components/Form";
 import { useState } from "react";
 import { useLocation } from "react-router-dom";
+import { useInfiniteQuery } from "hooks/useInfiniteQuery";
 
 export default function Movements() {
   const location = useLocation();
@@ -15,10 +16,6 @@ export default function Movements() {
 
   const [isFormOpen, setIsFormOpen] = useState(isFormOpenParam);
   const [search, setSearch] = useState("");
-
-  const movementsQuery = api.endpoints.readManyApiMovementsGet.useQuery({
-    search,
-  });
 
   const handleOpenCreateForm = () => {
     setIsFormOpen(true);
@@ -29,13 +26,16 @@ export default function Movements() {
   };
 
   const handleSearchChange = (value: string) => {
+    infiniteQuery.reset();
     setSearch(value);
   };
 
-  if (movementsQuery.isLoading) return <Loader active size="huge" />;
-
-  if (movementsQuery.isError)
-    return <QueryErrorMessage query={movementsQuery} />;
+  const infiniteQuery = useInfiniteQuery(
+    api.endpoints.readManyApiMovementsGet.useQuery,
+    { search },
+    10,
+    () => {}
+  );
 
   return (
     <>
@@ -45,11 +45,20 @@ export default function Movements() {
           search={search}
           onSearchChange={handleSearchChange}
         />
-        <FlexColumn.Auto style={{ padding: 1 }}>
-          {movementsQuery.isSuccess &&
-            movementsQuery.data.map((movement) => (
-              <Movement key={movement.id} movement={movement} />
-            ))}
+        <FlexColumn.Auto
+          style={{ padding: 1 }}
+          reference={infiniteQuery.reference}
+        >
+          <>
+            {infiniteQuery.isError && (
+              <QueryErrorMessage query={infiniteQuery} />
+            )}
+            {Object.values(infiniteQuery.pages).map((movements) =>
+              movements.map((movement) => (
+                <Movement key={movement.id} movement={movement} />
+              ))
+            )}
+          </>
         </FlexColumn.Auto>
       </FlexColumn>
       <Form onClose={handleCloseForm} open={isFormOpen} />
