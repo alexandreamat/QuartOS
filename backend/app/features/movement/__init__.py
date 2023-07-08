bd725095420d2e7ded7b6b1b1eaf0ad9064e81b1
Alexandre Amat <alex.amat@i-drc.com>
2023-07-08 16:12:39 +0200

    return transactions nested in movement, movement exceptions

--- backend/app/features/movement/__init__.py
+++ backend/app/features/movement/__init__.py
@@ -2,3 +2,4 @@ from .models import Movement, MovementApiIn, MovementApiOut
 from .crud import CRUDMovement
 from .plaid import sync_transactions
 from .api import MOVEMENTS
+from .exceptions import MovementNotFound
