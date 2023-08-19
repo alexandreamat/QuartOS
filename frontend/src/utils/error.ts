1918e3c95c2dd2cb9172b13f58d7c2ce6b59c09a
Alexandre Amat <alex.amat@i-drc.com>
2023-08-19 11:53:31 +0200

    more detailed error message

--- frontend/src/utils/error.ts
+++ frontend/src/utils/error.ts
@@ -2,33 +2,45 @@ import { SerializedError } from "@reduxjs/toolkit";
 import { FetchBaseQueryError } from "@reduxjs/toolkit/dist/query";
 import { SimpleQuery } from "interfaces";
 
-export function renderErrorMessage(error: FetchBaseQueryError | SerializedError | string) {
-    console.error(error)
-    if (typeof error === 'string' ) return error;
-    if (typeof error === 'object') {
-        if ('status' in error && error.status === 401) {
-            return 'Unauthorized access. Please login again.';
-        }
-        if ('data' in error) {
-            if (typeof error.data === 'string') return error.data;
-            if (typeof error.data === 'object' && error.data !== null) {
-                if ('detail' in error.data && typeof error.data.detail === 'string') {
-                    return error.data.detail;
-                }
-            }
-        }
-        if ('error' in error && typeof error.error === 'string') {
-            return error.error;
-        }
-        if ('message' in error && typeof error.message === 'string') {
-            return error.message;
-        }
+export function renderErrorMessage(
+  error: FetchBaseQueryError | SerializedError | string
+) {
+  console.error(error);
+  if (typeof error === "string") return error;
+  if (typeof error === "object") {
+    if ("status" in error && error.status === 401) {
+      return "Unauthorized access. Please login again.";
     }
-    return 'An error occurred. Please try again later.';
+    if ("data" in error) {
+      if (typeof error.data === "string") return error.data;
+      if (typeof error.data === "object" && error.data !== null) {
+        if ("detail" in error.data) {
+          if (typeof error.data.detail === "string") {
+            return error.data.detail;
+          } else if (Array.isArray(error.data.detail)) {
+            for (const item of error.data.detail) {
+              if (typeof item === "object") {
+                if ("msg" in item && typeof item.msg === "string") {
+                  return item.msg;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    if ("error" in error && typeof error.error === "string") {
+      return error.error;
+    }
+    if ("message" in error && typeof error.message === "string") {
+      return error.message;
+    }
+  }
+  return "An error occurred. Please try again later.";
 }
 
 export function logMutationError(error: unknown, query: SimpleQuery) {
-    console.error(error);
-    console.error(query.error);
-    console.error(query.originalArgs);
-  }
+  console.error(error);
+  console.error(query.error);
+  console.error(query.originalArgs);
+}
