84c54569223fe62986a6c2200805f12a1801e43f
Alexandre Amat <alex.amat@i-drc.com>
2023-08-28 23:53:13 +0200

    restyle pl

--- frontend/src/features/pl/index.tsx
+++ frontend/src/features/pl/index.tsx
@@ -1,10 +1,9 @@
 import { PlStatement, api } from "app/services/api";
-import ActionButton from "components/ActionButton";
-import CurrencyLabel from "components/CurrencyLabel";
 import { QueryErrorMessage } from "components/QueryErrorMessage";
-import { format } from "date-fns";
 import { useNavigate } from "react-router-dom";
-import { Header, Loader, Table } from "semantic-ui-react";
+import { Card, Loader, Table } from "semantic-ui-react";
+import PLCard from "./components/PLCard";
+import FlexColumn from "components/FlexColumn";
 
 function Row(props: { plStatement: PlStatement }) {
   const navigate = useNavigate();
@@ -14,40 +13,8 @@ function Row(props: { plStatement: PlStatement }) {
       `/pl-statements/${props.plStatement.start_date}/${props.plStatement.end_date}`
     );
   }
-
   return (
-    <Table.Row>
-      <Table.Cell>
-        <Header as="h5">
-          {format(new Date(props.plStatement.start_date), "MMMM yyyy")}
-        </Header>
-      </Table.Cell>
-      <Table.Cell>
-        <CurrencyLabel
-          amount={props.plStatement.income}
-          currencyCode={props.plStatement.currency_code}
-        />
-      </Table.Cell>
-      <Table.Cell>
-        <CurrencyLabel
-          amount={props.plStatement.expenses}
-          currencyCode={props.plStatement.currency_code}
-        />
-      </Table.Cell>
-      <Table.Cell>
-        <CurrencyLabel
-          amount={props.plStatement.income + props.plStatement.expenses}
-          currencyCode={props.plStatement.currency_code}
-        />
-      </Table.Cell>
-      <Table.Cell>
-        <ActionButton
-          onClick={handleGoToDetail}
-          tooltip="Detail"
-          icon="ellipsis horizontal"
-        />
-      </Table.Cell>
-    </Table.Row>
+    <PLCard aggregate={props.plStatement} onGoToDetail={handleGoToDetail} />
   );
 }
 
@@ -57,28 +24,21 @@ export default function IncomeStatement() {
       currencyCode: "EUR",
     });
 
-  if (aggregatesQuery.isLoading) return <Loader active size="huge" />;
+  if (aggregatesQuery.isUninitialized || aggregatesQuery.isLoading)
+    return <Loader active size="huge" />;
 
   if (aggregatesQuery.isError)
     return <QueryErrorMessage query={aggregatesQuery} />;
 
-  if (aggregatesQuery.isSuccess)
-    return (
-      <Table>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>Month</Table.HeaderCell>
-            <Table.HeaderCell>Income</Table.HeaderCell>
-            <Table.HeaderCell>Expenses</Table.HeaderCell>
-            <Table.HeaderCell>Net income</Table.HeaderCell>
-            <Table.HeaderCell>Actions</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
+  return (
+    <FlexColumn>
+      <FlexColumn.Auto>
+        <Card.Group style={{ margin: 0 }}>
           {aggregatesQuery.data.map((aggregate) => (
             <Row key={aggregate.start_date} plStatement={aggregate} />
           ))}
-        </Table.Body>
-      </Table>
-    );
+        </Card.Group>
+      </FlexColumn.Auto>
+    </FlexColumn>
+  );
 }
