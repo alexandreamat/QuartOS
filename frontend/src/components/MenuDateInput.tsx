bfaf1616ebe1c6a25f8c74e466970eb6415af4bb
Alexandre Amat <alex.amat@hotmail.com>
2023-09-03 13:59:52 +0200

    menu with popups

--- frontend/src/components/MenuDateInput.tsx
+++ frontend/src/components/MenuDateInput.tsx
@@ -1,4 +1,10 @@
-import { Input, InputOnChangeData, Menu } from "semantic-ui-react";
+import {
+  Button,
+  Input,
+  InputOnChangeData,
+  Menu,
+  Popup,
+} from "semantic-ui-react";
 import { format } from "date-fns";
 import { stringToDate } from "utils/time";
 import { ClickableIcon } from "./ClickableIcon";
@@ -11,16 +17,23 @@ export default function MenuDateInput(props: {
   const dateStr = props.date ? format(props.date, "yyyy-MM-dd") : "";
   return (
     <Menu.Item fitted>
-      <Input
-        label={props.label}
-        type="date"
-        value={dateStr}
-        onChange={(_: unknown, data: InputOnChangeData) => {
-          const newDate = stringToDate(data.value);
-          if (isNaN(newDate.getTime())) return;
-          props.onDateChange(newDate);
-        }}
-      />
+      <Popup
+        trigger={<Button icon="calendar" />}
+        hoverable
+        position="bottom right"
+        on="click"
+      >
+        <Input
+          label={props.label}
+          type="date"
+          value={dateStr}
+          onChange={(_: unknown, data: InputOnChangeData) => {
+            const newDate = stringToDate(data.value);
+            if (isNaN(newDate.getTime())) return;
+            props.onDateChange(newDate);
+          }}
+        />
+      </Popup>
       {props.date && (
         <Menu.Item fitted>
           <ClickableIcon
--- frontend/src/features/transaction/components/Bar.tsx
+++ frontend/src/features/transaction/components/Bar.tsx
@@ -1,12 +1,14 @@
 import { useAccountOptions } from "features/account/hooks";
 import {
   Button,
+  Divider,
   Dropdown,
   DropdownProps,
   Icon,
   Input,
   InputOnChangeData,
   Menu,
+  Popup,
 } from "semantic-ui-react";
 import MenuDateInput from "components/MenuDateInput";
 import { ClickableIcon } from "components/ClickableIcon";
@@ -31,8 +33,8 @@ export default function Bar(props: {
   const amountLeStr = props.amountLe !== undefined ? props.amountLe : "";
 
   return (
-    <Menu secondary style={{ width: "100%" }}>
-      <Menu.Item>
+    <Menu secondary>
+      <Menu.Item fitted style={{ flex: 1 }}>
         <Input
           icon="search"
           placeholder="Search..."
@@ -43,22 +45,26 @@ export default function Bar(props: {
           ) => props.onSearchChange(data.value as string)}
         />
       </Menu.Item>
-      <Menu.Item>
-        <Dropdown
-          icon="filter"
-          labeled
-          className="icon"
-          button
-          placeholder="Filter by account"
-          search
-          selection
-          value={props.accountId}
-          options={accountOptions.data || []}
-          onChange={(
-            event: React.SyntheticEvent<HTMLElement>,
-            data: DropdownProps
-          ) => props.onAccountIdChange(data.value as number)}
-        />
+      <Menu.Item fitted>
+        <Popup
+          trigger={<Button icon="filter" />}
+          hoverable
+          position="bottom right"
+          on="click"
+        >
+          <Dropdown
+            button
+            placeholder="Filter by account"
+            search
+            selection
+            value={props.accountId}
+            options={accountOptions.data || []}
+            onChange={(
+              event: React.SyntheticEvent<HTMLElement>,
+              data: DropdownProps
+            ) => props.onAccountIdChange(data.value as number)}
+          />
+        </Popup>
         {props.accountId !== 0 && (
           <Menu.Item fitted>
             <ClickableIcon
@@ -68,75 +74,84 @@ export default function Bar(props: {
           </Menu.Item>
         )}
       </Menu.Item>
-      <Menu.Item>
-        <MenuDateInput
-          label="from"
-          date={props.timestamp}
-          onDateChange={props.onTimestampChange}
-        />
-        <Menu.Item fitted>
-          <Button icon onClick={props.onToggleIsDescending}>
-            <Icon
-              name={props.isDescending ? "sort amount down" : "sort amount up"}
+      <MenuDateInput
+        label="from"
+        date={props.timestamp}
+        onDateChange={props.onTimestampChange}
+      />
+
+      <Menu.Item fitted>
+        <Popup
+          trigger={<Button icon="dollar" />}
+          hoverable
+          position="bottom right"
+          on="click"
+        >
+          <Input
+            label=">="
+            type="number"
+            input={{
+              inputMode: "decimal",
+              step: "0.01",
+            }}
+            placeholder="Amount from"
+            value={amountGeStr}
+            onChange={(_, data) => {
+              if (data.value.length) {
+                const newAmount = Number(data.value);
+                if (isNaN(newAmount)) return;
+                props.onAmountGeChange(newAmount);
+              } else {
+                props.onAmountGeChange(undefined);
+              }
+            }}
+          />
+          {props.amountGe !== undefined && props.amountLe !== undefined ? (
+            <Divider horizontal>
+              {props.amountGe < props.amountLe ? "and" : "or"}
+            </Divider>
+          ) : (
+            <Divider />
+          )}
+
+          <Input
+            label="<="
+            type="number"
+            input={{
+              inputMode: "decimal",
+              step: "0.01",
+            }}
+            placeholder="Amount to"
+            value={amountLeStr}
+            onChange={(_, data) => {
+              if (data.value.length) {
+                const newAmount = Number(data.value);
+                if (isNaN(newAmount)) return;
+                props.onAmountLeChange(newAmount);
+              } else {
+                props.onAmountLeChange(undefined);
+              }
+            }}
+          />
+        </Popup>
+        {(props.amountGe !== undefined || props.amountLe !== undefined) && (
+          <Menu.Item fitted>
+            <ClickableIcon
+              name="remove circle"
+              onClick={() => {
+                props.onAmountGeChange(undefined);
+                props.onAmountLeChange(undefined);
+              }}
             />
-          </Button>
-        </Menu.Item>
+          </Menu.Item>
+        )}
       </Menu.Item>
-      <Menu.Item>
-        <Input
-          label="from"
-          type="number"
-          input={{
-            inputMode: "decimal",
-            step: "0.01",
-          }}
-          placeholder="Amount from"
-          value={amountGeStr}
-          onChange={(_, data) => {
-            if (data.value.length) {
-              const newAmount = Number(data.value);
-              if (isNaN(newAmount)) return;
-              props.onAmountGeChange(newAmount);
-            } else {
-              props.onAmountGeChange(undefined);
-            }
-          }}
-        />
-        {props.amountGe !== undefined && (
-          <Menu.Item fitted>
-            <ClickableIcon
-              name="remove circle"
-              onClick={() => props.onAmountGeChange(undefined)}
-            />
-          </Menu.Item>
-        )}
-        <Input
-          label="to"
-          type="number"
-          input={{
-            inputMode: "decimal",
-            step: "0.01",
-          }}
-          placeholder="Amount to"
-          value={amountLeStr}
-          onChange={(_, data) => {
-            if (data.value.length) {
-              const newAmount = Number(data.value);
-              if (isNaN(newAmount)) return;
-              props.onAmountLeChange(newAmount);
-            } else {
-              props.onAmountLeChange(undefined);
-            }
-          }}
-        />
-        {props.amountLe !== undefined && (
-          <Menu.Item fitted>
-            <ClickableIcon
-              name="remove circle"
-              onClick={() => props.onAmountLeChange(undefined)}
-            />
-          </Menu.Item>
-        )}
+      <Menu.Item fitted>
+        <Button icon onClick={props.onToggleIsDescending}>
+          <Icon
+            name={props.isDescending ? "sort amount down" : "sort amount up"}
+          />
+        </Button>
       </Menu.Item>
     </Menu>
   );
