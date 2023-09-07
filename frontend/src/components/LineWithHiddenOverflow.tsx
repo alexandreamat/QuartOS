f3cd5a6246e8c418620f8b7b33f00d75a87295ac
Alexandre Amat <alex.amat@hotmail.com>
2023-09-07 12:32:00 +0200

    fixup

--- frontend/src/components/LineWithHiddenOverflow.tsx
+++ frontend/src/components/LineWithHiddenOverflow.tsx
@@ -1,10 +1,16 @@
-export default function LineWithHiddenOverflow(props: { content: string }) {
+import { CSSProperties } from "react";
+
+export default function LineWithHiddenOverflow(props: {
+  content: string;
+  style?: CSSProperties;
+}) {
   return (
     <p
       style={{
         whiteSpace: "nowrap",
         overflow: "hidden",
         textOverflow: "ellipsis",
+        ...props.style,
       }}
     >
       {props.content}
