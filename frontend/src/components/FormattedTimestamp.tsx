f57d5183bdda1da035560a4d9dca9136f1e6b1cb
Alexandre Amat <alex.amat@i-drc.com>
2023-09-07 12:31:33 +0200

    remove grid from cards, use flex row

--- frontend/src/components/FormattedTimestamp.tsx
+++ frontend/src/components/FormattedTimestamp.tsx
@@ -2,7 +2,7 @@ import { format } from "date-fns";
 
 export default function FormattedTimestamp(props: { timestamp?: string }) {
   return (
-    <p>
+    <p style={{ width: "10em" }}>
       {props.timestamp && format(new Date(props.timestamp), " yyyy MMMM d")}
     </p>
   );
