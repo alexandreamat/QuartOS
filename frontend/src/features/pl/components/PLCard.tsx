eebec6b00e17da5653a5cf6c87155e64d22781cb
Alexandre Amat <alex.amat@i-drc.com>
2023-09-14 16:19:32 +0200

    fixup

--- frontend/src/features/pl/components/PLCard.tsx
+++ frontend/src/features/pl/components/PLCard.tsx
@@ -2,42 +2,59 @@ import { PlStatement } from "app/services/api";
 import ActionButton from "components/ActionButton";
 import CurrencyLabel from "components/CurrencyLabel";
 import FlexRow from "components/FlexRow";
-import Inline from "components/Inline";
 import { addDays, format } from "date-fns";
 import { Card, Header, Label, Placeholder, Step } from "semantic-ui-react";
 
 export default function PLCard(props: {
-  aggregate: PlStatement;
+  aggregate?: PlStatement;
   showIncome?: boolean;
   onGoToDetail?: () => void;
   onClickIncome?: () => void;
   onClickExpenses?: () => void;
+  loading?: boolean;
 }) {
-  const startDate = new Date(props.aggregate.start_date);
-  const endDate = new Date(props.aggregate.end_date);
+  const startDate = props.aggregate && new Date(props.aggregate.start_date);
+  const endDate = props.aggregate && new Date(props.aggregate.end_date);
   const today = new Date();
-  const isOngoing = startDate <= today && today <= endDate;
+  const isOngoing =
+    startDate && endDate && startDate <= today && today <= endDate;
 
   return (
     <Card fluid color="teal">
       <Card.Content>
         <Card.Header>
           <FlexRow style={{ justifyContent: "space-between" }}>
-            {format(new Date(props.aggregate.start_date), "MMMM yyyy")}
+            {props.loading ? (
+              <Placeholder style={{ width: "100%" }}>
+                <Placeholder.Line />
+              </Placeholder>
+            ) : (
+              props.aggregate &&
+              format(new Date(props.aggregate.start_date), "MMMM yyyy")
+            )}
             {props.onGoToDetail && (
               <ActionButton
                 onClick={props.onGoToDetail}
                 icon="file alternate outline"
                 content="See Report"
+                loading={props.loading}
               />
             )}
           </FlexRow>
         </Card.Header>
         <Card.Meta>
-          {`From ${startDate.toLocaleDateString()} to ${addDays(
-            endDate,
-            -1
-          ).toLocaleDateString()}`}
+          {props.loading ? (
+            <Placeholder>
+              <Placeholder.Line />
+            </Placeholder>
+          ) : (
+            startDate &&
+            endDate &&
+            `From ${startDate.toLocaleDateString()} to ${addDays(
+              endDate,
+              -1,
+            ).toLocaleDateString()}`
+          )}
           {isOngoing && (
             <Label
               color="teal"
@@ -47,7 +64,7 @@ export default function PLCard(props: {
             </Label>
           )}
         </Card.Meta>
-        <Step.Group fluid widths={3}>
+        <Step.Group fluid widths={2}>
           <Step
             onClick={props.onClickIncome}
             active={props.showIncome !== undefined ? props.showIncome : false}
@@ -55,8 +72,9 @@ export default function PLCard(props: {
             <Step.Title>Income</Step.Title>
             <Step.Content>
               <CurrencyLabel
-                amount={props.aggregate.income}
-                currencyCode={props.aggregate.currency_code}
+                amount={props.aggregate?.income}
+                currencyCode={props.aggregate?.currency_code}
+                loading={props.loading}
               />
             </Step.Content>
           </Step>
@@ -67,8 +85,9 @@ export default function PLCard(props: {
             <Step.Title>Expenses</Step.Title>
             <Step.Content>
               <CurrencyLabel
-                amount={props.aggregate.expenses}
-                currencyCode={props.aggregate.currency_code}
+                amount={props.aggregate?.expenses}
+                currencyCode={props.aggregate?.currency_code}
+                loading={props.loading}
               />
             </Step.Content>
           </Step>
@@ -79,8 +98,12 @@ export default function PLCard(props: {
           <Header as="h5">
             Net Income:
             <CurrencyLabel
-              amount={props.aggregate.expenses + props.aggregate.income}
-              currencyCode={props.aggregate.currency_code}
+              amount={
+                props.aggregate &&
+                props.aggregate.expenses + props.aggregate.income
+              }
+              currencyCode={props.aggregate?.currency_code}
+              loading={props.loading}
             />
           </Header>
         </FlexRow>
@@ -88,55 +111,3 @@ export default function PLCard(props: {
     </Card>
   );
 }
-
-function PLCardPlaceholder(props: { onGoToDetail?: boolean }) {
-  return (
-    <Card fluid color="teal">
-      <Card.Content>
-        <Card.Header>
-          <FlexRow style={{ justifyContent: "space-between" }}>
-            <Placeholder style={{ width: "100%" }}>
-              <Placeholder.Line />
-            </Placeholder>
-            {props.onGoToDetail && (
-              <ActionButton
-                icon="file alternate outline"
-                content="See Report"
-                loading
-              />
-            )}
-          </FlexRow>
-        </Card.Header>
-        <Card.Meta>
-          <Placeholder>
-            <Placeholder.Line />
-          </Placeholder>
-        </Card.Meta>
-        <Step.Group fluid widths={3}>
-          <Step>
-            <Step.Title>Income</Step.Title>
-            <Step.Content>
-              <CurrencyLabel loading />
-            </Step.Content>
-          </Step>
-          <Step>
-            <Step.Title>Expenses</Step.Title>
-            <Step.Content>
-              <CurrencyLabel loading />
-            </Step.Content>
-          </Step>
-        </Step.Group>
-      </Card.Content>
-      <Card.Content extra>
-        <Inline justifyContent="right">
-          <Header as="h5">
-            Net Income:
-            <CurrencyLabel loading />
-          </Header>
-        </Inline>
-      </Card.Content>
-    </Card>
-  );
-}
-
-PLCard.Placeholder = PLCardPlaceholder;
