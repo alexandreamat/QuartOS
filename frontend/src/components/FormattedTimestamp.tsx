cdebf615e278532a97ceb23a5fc0d6c8be222e55
Alexandre Amat <alex.amat@i-drc.com>
2023-09-18 14:02:44 +0200

    transaction cards ui bugs

--- frontend/src/components/FormattedTimestamp.tsx
+++ frontend/src/components/FormattedTimestamp.tsx
@@ -1,21 +1,23 @@
 import { format } from "date-fns";
+import { CSSProperties } from "react";
 import { Placeholder } from "semantic-ui-react";
 
 export default function FormattedTimestamp(props: {
   timestamp?: string;
   loading?: boolean;
+  style?: CSSProperties;
 }) {
   if (props.loading)
     return (
       <Placeholder>
         <Placeholder.Header>
-          <Placeholder.Line style={{ width: "9em" }} />
+          <Placeholder.Line style={props.style} />
         </Placeholder.Header>
       </Placeholder>
     );
 
   return (
-    <p style={{ width: "9em" }}>
+    <p style={props.style}>
       {props.timestamp && format(new Date(props.timestamp), " yyyy MMMM d")}
     </p>
   );
