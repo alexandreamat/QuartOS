70df854bbe9b220ec07ae6ad637f29d7e4fa6b75
Alexandre Amat <alex.amat@i-drc.com>
2023-12-05 16:58:24 -0800

    exchange rate exception management

--- backend/app/api/exchangerate.py
+++ backend/app/api/exchangerate.py
@@ -1,15 +1,15 @@
 # Copyright (C) 2023 Alexandre Amat
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -28,7 +28,5 @@ router = APIRouter()
 def read_exchange_rate(from_currency: str, to_currency: str, date: date) -> Decimal:
     try:
         return get_exchange_rate(from_currency, to_currency, date)
-    except requests.HTTPError as exc:
-        raise HTTPException(status_code=exc.response.status_code)
-    except KeyError:
+    except requests.HTTPError:
         raise HTTPException(status.HTTP_400_BAD_REQUEST)
