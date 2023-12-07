498913495fa8b42cd319e33334cca71d751855a8
Alexandre Amat <alex.amat@i-drc.com>
2023-12-06 17:08:43 -0800

    base url

--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -13,4 +13,4 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-export const BASE_URL = "https://quartos-api.alexamat.com";
+export const BASE_URL = "https://quartos.alexamat.com/api";
