657681f96506e30a67b54fb4e396d1ba875a8c1e
Alexandre Amat <alex.amat@i-drc.com>
2023-12-30 20:27:32 -0800

    rm prints

--- backend/app/common/plaid.py
+++ backend/app/common/plaid.py
@@ -17,19 +17,18 @@ import os
 
 import plaid
 from plaid.api.plaid_api import PlaidApi
-from plaid.model.products import Products
-from plaid.model.link_token_create_request import LinkTokenCreateRequest
-from plaid.model.link_token_create_response import LinkTokenCreateResponse
-from plaid.model.link_token_create_request_user import LinkTokenCreateRequestUser
+from plaid.model.country_code import CountryCode
 from plaid.model.item_public_token_exchange_request import (
     ItemPublicTokenExchangeRequest,
 )
 from plaid.model.item_public_token_exchange_response import (
     ItemPublicTokenExchangeResponse,
 )
+from plaid.model.link_token_create_request import LinkTokenCreateRequest
 from plaid.model.link_token_create_request_update import LinkTokenCreateRequestUpdate
-from plaid.model.country_code import CountryCode
-
+from plaid.model.link_token_create_request_user import LinkTokenCreateRequestUser
+from plaid.model.link_token_create_response import LinkTokenCreateResponse
+from plaid.model.products import Products
 
 PLAID_CLIENT_ID = os.environ["PLAID_CLIENT_ID"]
 PLAID_SECRET = os.environ["PLAID_SECRET"]
@@ -46,9 +45,6 @@ match PLAID_ENV:
         host = plaid.Environment.Production
     case _:
         exit()
-
-print(PLAID_ENV)
-print(PLAID_SECRET)
 
 configuration = plaid.Configuration(
     host=host,
