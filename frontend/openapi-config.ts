15c96121da93e1ee6a35aa31379a2a94a79d4210
Alexandre Amat <alex.amat@hotmail.com>
2024-01-09 23:58:10 -0800

    regenerate api

--- frontend/openapi-config.ts
+++ frontend/openapi-config.ts
@@ -2,7 +2,7 @@ import type { ConfigFile } from "@rtk-query/codegen-openapi";
 
 const config: ConfigFile = {
   apiFile: "./src/app/services/emptyApi.ts",
-  schemaFile: "http://localhost:8000/api/openapi.json",
+  schemaFile: "http://localhost:8000/openapi.json",
   apiImport: "emptySplitApi",
   outputFile: "./src/app/services/generatedApi.ts",
   exportName: "generatedApi",
