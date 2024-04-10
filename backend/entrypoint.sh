42c28ff723f7ef703244c5f68963aaf1073145a4
Yair Bonastre <yair.bonastre@sita.aero>
2024-04-10 19:07:36 +1200

    updating missing copyrights from 2023 to 2024

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (C) 2023 Alexandre Amat
+# Copyright (C) 2024 Alexandre Amat
 # 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
